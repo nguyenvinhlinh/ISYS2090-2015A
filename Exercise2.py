69c511fe562fc80515b6f9423bcba221a26d17b5
B.IT User <bit@db-concepts>
2015-05-04 13:03:59 +0700

    function checkfileSQL done

--- Exercise2.py
+++ Exercise2.py
@@ -1,2 +1,17 @@
 __author__ = 'Nguyen Vinh Linh & Nguyen Duy Kien'
 
+def checkFileSQL():
+    fileName = raw_input("Enter file name: ")
+    if fileName[-4:] != ".sql":
+        print("Invalid choosen file. File must be .sql")
+        return False
+    try:
+        thefile = open(fileName, "r")
+        return True
+    except IOError as ex:
+        print("File not found")
+        return False
+
+
+validFile = checkFileSQL()
+print("return value: "+str(validFile))
